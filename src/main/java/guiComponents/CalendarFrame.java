56a7e552e1c777730cc26877177bb7dfa80eb199
Eometheous <jonathan.sthomas@icloud.com>
2023-04-22 09:36:51 -0700

    Added a menu bar with various options

--- src/main/java/guiComponents/CalendarFrame.java
+++ src/main/java/guiComponents/CalendarFrame.java
@@ -5,10 +5,11 @@ import calendar.MyCalendar;
 import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CalendarFrame extends JFrame {
     public CalendarFrame(MyCalendar calendar) {
-
+        super("My Calendar");
         Model<LocalDate> dateModel = new Model<>();
         Model<String> stringModel = new Model<>();
         stringModel.add(calendar.displayTodaysEvents());
@@ -27,11 +28,40 @@ public class CalendarFrame extends JFrame {
 
         JMenuItem addEventButton = new JMenuItem("Add Event");
         eventsMenu.add(addEventButton);
+        addEventButton.addActionListener(e -> new AddEventFrame("Creating Event", dateModel, stringModel, calendar));
 
         JMenuItem removeEventButton = new JMenuItem("Remove Event");
         eventsMenu.add(removeEventButton);
 
+        JMenu navigateMenu = new JMenu("Navigate");
+        JMenuItem goToToday = new JMenuItem("Go To Today");
+        goToToday.addActionListener(e -> {
+            dateModel.update(0, LocalDate.now());
+            stringModel.update(0,calendar.displaySelectedDay());
+        });
+        navigateMenu.add(goToToday);
 
+        JMenuItem goToDate = new JMenuItem("Go To...");
+        goToDate.addActionListener(e -> {
+            DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("M/d/yyyy");
+            String stringDate = JOptionPane.showInputDialog(this, "Enter Date (mm/dd/yyyy): ");
+            if (stringDate != null) {
+                LocalDate date = LocalDate.parse(stringDate, monthDayYear);
+                dateModel.update(0, date);
+                stringModel.update(0, calendar.displaySelectedDay());
+            }
+        });
+        navigateMenu.add(add(goToDate));
+
+        JMenu view = new JMenu("View");
+
+        JMenuItem viewAllEvents = new JMenuItem("View All Events");
+        viewAllEvents.addActionListener(e -> stringModel.update(0,calendar.displayEventsList()));
+        view.add(viewAllEvents);
+
+        menuBar.add(view);
+
+        menuBar.add(navigateMenu);
 
         add(menuBar, BorderLayout.NORTH);
 

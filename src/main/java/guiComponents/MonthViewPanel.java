c04430c02a743ecca6ffc5f55a8850446b128a9e
Eometheous <jonathan.sthomas@icloud.com>
2023-04-23 21:58:22 -0700

    Added JavaDocs

--- src/main/java/guiComponents/MonthViewPanel.java
+++ src/main/java/guiComponents/MonthViewPanel.java
@@ -5,13 +5,25 @@ import calendar.MyCalendar;
 import javax.swing.*;
 import java.time.LocalDate;
 
+/**
+ * The month view for the calendar which contains buttons for each day of the month, buttons to change the date, and a
+ * label for displaying the month and year
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230423
+ */
 public class MonthViewPanel extends JPanel {
+    /**
+     * The constructor for this panel
+     * @param dateModel     model containing the selected date
+     * @param stringModel   model containing the string for the day view
+     * @param calendar      the calendar being used
+     */
     public MonthViewPanel(Model<LocalDate> dateModel, Model<String> stringModel, MyCalendar calendar) {
         dateModel.add(calendar.getToday());
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-        ButtonsPanel buttonsPanel = new ButtonsPanel(calendar, dateModel, stringModel);
-        add(buttonsPanel);
+        MonthViewControllerPanel monthViewControllerPanel = new MonthViewControllerPanel(calendar, dateModel, stringModel);
+        add(monthViewControllerPanel);
 
         MonthViewTextPanel monthViewTextPanel = new MonthViewTextPanel(dateModel);
         add(monthViewTextPanel);

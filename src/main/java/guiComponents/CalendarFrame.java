6bf9e075f635d426d33c59f334bc16a7496f7f64
Eometheous <jonathan.sthomas@icloud.com>
2023-04-23 21:32:55 -0700

    Extracted menu bar code into a method Added JavaDocs

--- src/main/java/guiComponents/CalendarFrame.java
+++ src/main/java/guiComponents/CalendarFrame.java
@@ -7,7 +7,17 @@ import java.awt.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The frame for the calendar. It contains the month view on the left,
+ * a day view on the right, and a menu bar at the top.
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230423
+ */
 public class CalendarFrame extends JFrame {
+    /**
+     * The constructor for the calendar frame
+     * @param calendar  the calendar being used in this frame
+     */
     public CalendarFrame(MyCalendar calendar) {
         super("My Calendar");
         Model<LocalDate> dateModel = new Model<>();
@@ -22,17 +32,34 @@ public class CalendarFrame extends JFrame {
         stringModel.attach(dayViewPanel);
         add(dayViewPanel, BorderLayout.EAST);
 
+        JMenuBar menuBar = createMenuBar(calendar, dateModel, stringModel);
+
+        add(menuBar, BorderLayout.NORTH);
+
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+    }
+
+    /**
+     * Creates and returns a menu bar to be used within the calendar
+     * @param calendar      the calendar being used
+     * @param dateModel     the model containing the selected date
+     * @param stringModel   model containing the string for the day view
+     * @return              the menu bar
+     */
+    private JMenuBar createMenuBar(MyCalendar calendar, Model<LocalDate> dateModel, Model<String> stringModel) {
         JMenuBar menuBar = new JMenuBar();
         JMenu eventsMenu = new JMenu("Events");
         menuBar.add(eventsMenu);
 
         JMenuItem addEventButton = new JMenuItem("Add Event");
         eventsMenu.add(addEventButton);
-        addEventButton.addActionListener(e -> new AddEventFrame("Creating Event", dateModel, stringModel, calendar));
+        addEventButton.addActionListener(e -> new AddEventFrame(dateModel, stringModel, calendar));
 
         JMenu removeEventMenu = new JMenu("Remove Event");
         JMenuItem removeSelected = new JMenuItem("Remove a Selected Event");
-        removeSelected.addActionListener(e -> new RemoveEventFrame("Removing Event", dateModel, stringModel, calendar));
+        removeSelected.addActionListener(e -> new RemoveEventFrame(dateModel, stringModel, calendar));
 
         JMenuItem removeAll = new JMenuItem("Remove All Events");
         removeAll.addActionListener(e -> {
@@ -51,13 +78,13 @@ public class CalendarFrame extends JFrame {
                 LocalDate date = LocalDate.parse(stringDate, monthDayYear);
                 calendar.deleteAllEventsOn(date);
                 dateModel.pingListeners();
-                stringModel.update(0,calendar.displaySelectedDay());
+                stringModel.update(0, calendar.displaySelectedDay());
             }
         });
 
         JMenuItem removeRecurringEvent = new JMenuItem("Remove a Recurring Event");
         removeRecurringEvent.addActionListener(e -> new
-                RemoveRecurringEvent("Remove Recurring Event", dateModel, stringModel, calendar));
+                RemoveRecurringEvent(dateModel, stringModel, calendar));
 
         removeEventMenu.add(removeSelected);
         removeEventMenu.add((removeAll));
@@ -69,7 +96,7 @@ public class CalendarFrame extends JFrame {
         JMenuItem goToToday = new JMenuItem("Go To Today");
         goToToday.addActionListener(e -> {
             dateModel.update(0, LocalDate.now());
-            stringModel.update(0,calendar.displaySelectedDay());
+            stringModel.update(0, calendar.displaySelectedDay());
         });
         navigateMenu.add(goToToday);
 
@@ -88,17 +115,12 @@ public class CalendarFrame extends JFrame {
         JMenu view = new JMenu("View");
 
         JMenuItem viewAllEvents = new JMenuItem("View All Events");
-        viewAllEvents.addActionListener(e -> stringModel.update(0,calendar.displayEventsList()));
+        viewAllEvents.addActionListener(e -> stringModel.update(0, calendar.displayEventsList()));
         view.add(viewAllEvents);
 
         menuBar.add(view);
 
         menuBar.add(navigateMenu);
-
-        add(menuBar, BorderLayout.NORTH);
-
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
+        return menuBar;
     }
 }

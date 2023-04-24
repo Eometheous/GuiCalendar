ffd61cb2ceabafefd5bdb6797a7745c4f6acde7a
Eometheous <jonathan.sthomas@icloud.com>
2023-04-23 21:34:18 -0700

    Added JavaDocs

--- src/main/java/guiComponents/DayButton.java
+++ src/main/java/guiComponents/DayButton.java
@@ -3,13 +3,28 @@ package guiComponents;
 import javax.swing.*;
 import java.time.LocalDate;
 
+/**
+ * A button containing a date
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.2304023
+ */
 public class DayButton extends JButton {
     LocalDate date;
+
+    /**
+     * The constructor for the day button
+     * @param name  The name of the button
+     * @param date  The date the button is holding
+     */
     public DayButton(String name, LocalDate date) {
         super(name);
         this.date = date;
     }
 
+    /**
+     * Returns the date that this button is holding
+     * @return  the date
+     */
     public LocalDate getDate() {
         return date;
     }

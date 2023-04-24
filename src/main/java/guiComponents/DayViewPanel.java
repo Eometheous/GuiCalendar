ef9acc7e0e72c293013df589e7e4e4e10bd3c7f1
Eometheous <jonathan.sthomas@icloud.com>
2023-04-23 21:36:46 -0700

    Added JavaDocs

--- src/main/java/guiComponents/DayViewPanel.java
+++ src/main/java/guiComponents/DayViewPanel.java
@@ -5,9 +5,19 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
 
+/**
+ * The day view of the calendar which contains a text area
+ * @author Jonathan Stewart Thomas
+ * @version 1.0.0.230423
+ */
 public class DayViewPanel extends JPanel implements ChangeListener {
     Model<String> stringModel;
     JTextArea textArea;
+
+    /**
+     * The constructor for this panel
+     * @param stringModel   model containing the string for the day view
+     */
     public DayViewPanel(Model<String> stringModel) {
         this.stringModel = stringModel;
         textArea = new JTextArea(20, 45);
@@ -17,6 +27,11 @@ public class DayViewPanel extends JPanel implements ChangeListener {
         JScrollPane jScrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         add(jScrollPane);
     }
+
+    /**
+     * Updates the text area with the text from the string model
+     * @param e  a ChangeEvent object
+     */
     @Override
     public void stateChanged(ChangeEvent e) {
         textArea.setText(stringModel.get(0));
